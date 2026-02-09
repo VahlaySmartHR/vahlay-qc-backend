828ce17bab7c75a9c7afc97c769b0b62e2a4239a
VahlaySmartHR <vikram.saw@vahlayconsulting.com>
2026-02-09 14:49:58 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { authGuard } from "./middlewares/auth.middleware";
 import { qcRoutes } from "./routes/qc.routes";
 import { uploadRoutes } from "./routes/upload.routes";
+import { authGuard } from "./middlewares/auth.middleware";
 
 export const app = Fastify({ logger: true });
 
 app.register(cors);
+
+// health check (KEEP THIS)
+app.get("/", async () => {
+  return { status: "ok" };
+});
+
+// auth
 app.addHook("preHandler", authGuard);
 
+// routes
 app.register(uploadRoutes, { prefix: "/api/qc" });
 app.register(qcRoutes, { prefix: "/api/qc" });
